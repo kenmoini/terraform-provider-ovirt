a4de874c8dd82697c2387a345daf76e2ee0ae63b
Ken Moini <ken@kenmoini.com>
2020-12-30 01:37:06 -0500

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,16 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/ovirt/terraform-provider-ovirt/ovirt"
+	"github.com/kenmoini/terraform-provider-ovirt/ovirt"
+)
+
+var (
+	// these will be set by the goreleaser configuration
+	// to appropriate values for the compiled binary
+	version string = "dev"
+
+	// goreleaser can also pass the specific commit if you want
+	// commit  string = ""
 )
 
 func main() {
